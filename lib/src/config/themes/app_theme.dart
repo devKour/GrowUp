982f19192620153a6d749367993afa18901d687a
JiHoonAHN <ahnjh1028@naver.com>
2022-08-01 08:29:16 +0900

    [CREATE] Font

---
+++ lib/src/config/themes/app_theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData get light {
+    return ThemeData(
+        appBarTheme: const AppBarTheme(),
+        scaffoldBackgroundColor: Colors.white,
+        primaryColor: Colors.black,
+        accentColor: Colors.black,
+        splashColor: Colors.transparent,
+        fontFamily: '');
+  }
+}
---
+++ lib/src/core/utils/constants.dart
@@ -0,0 +1,1 @@
+const String growUpMaterialAppTitle = 'Grow Up';
